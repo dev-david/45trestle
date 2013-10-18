4d50d9201dce9d759ed31ea31f343e2e2513b6eb
david <simpleaznx@gmail.com>
2013-10-18 12:00:13 -0700

    phone number

--- index.php
+++ index.php
@@ -50,7 +50,8 @@
 								<li><a href="#portfolio" id="portfolio-link" class="skel-panels-ignoreHref"><span class="icon icon-th">Listing Services</span></a></li>
 								<li><a href="#about" id="about-link" class="skel-panels-ignoreHref"><span class="icon icon-dollar">Market Analysis</span></a></li>
 								<li><a href="#contact" id="contact-link" class="skel-panels-ignoreHref"><span class="icon icon-group">About Us</span></a></li>
-								<li><a href="#contact" id="contact-link" class="skel-panels-ignoreHref"><span class="icon icon-phone">Call - 510.545.6932</span></a></li>
+								<li><a href="#contact" id="contact-link" class="skel-panels-ignoreHref"><span class="icon icon-phone">Call Kenny - 510.545.6932</span></a></li>
+								<li><a href="#contact" id="contact-link" class="skel-panels-ignoreHref"><span class="icon icon-phone">Call Kevin - 510.995.0015</span></a></li>
 							</ul>
 						</nav>
 						
